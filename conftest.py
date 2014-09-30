72ef3a6eec277bcbb82c615e285177b6f41b7aee
Jindrich Smitka <jsmitka@smita.info>
2014-09-30 14:56:01 +0200

    Fixed test run on Django 1.7

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import os
+
+import django
 from django.conf import settings
 
 
 def pytest_configure():
+
     if not settings.configured:
         os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+    if django.VERSION > (1, 7):
+        django.setup()
--- tests/settings.py
+++ tests/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'south',
 
     'sales_menu',
 
